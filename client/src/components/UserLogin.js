e22eeba236e6daccf8102fc147ecf88a4500bb26
Sreeja Gaddameedi <88978257+Sreeja0410@users.noreply.github.com>
2024-11-28 01:16:52 +0530

    Update UserLogin.js

--- client/src/components/UserLogin.js
+++ client/src/components/UserLogin.js
@@ -18,7 +18,7 @@ const UserLogin = () => {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch('https://qna-web-app.vercel.app/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
